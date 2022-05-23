c03e9076744bb92e9646b81d8a306c66bc4996c7
Ahmed Tahir <ahmed.tahir@optimusfox.com>
2022-05-23 15:56:40 +0500

    REACT-ts-opt Module

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import OTPInput from "components/OTPInput";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      - Number OTP:
+      <OTPInput
+        autoFocus
+        isNumberInput
+        length={4}
+        className="otpContainer"
+        inputClassName="otpInput"
+        onChangeOTP={(otp) => console.log("Number OTP: ", otp)}
+      />
+      - String OTP:
+      <OTPInput
+        autoFocus
+        length={4}
+        className="otpContainer"
+        inputClassName="otpInput"
+        onChangeOTP={(otp) => console.log("String OTP: ", otp)}
+      />
+    </div>
+  );
+};
+
+export default App;
